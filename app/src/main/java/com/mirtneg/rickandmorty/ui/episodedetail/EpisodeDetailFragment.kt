6933f20ee5fd5e9c18c99d6c2b40d6e77d4a9f24
ArianMehana <mehanaarian@gmail.com>
2022-10-04 16:07:58 +0200

    added back function to go from the episode back to the character you had previously clicked.

--- app/src/main/java/com/mirtneg/rickandmorty/ui/episodedetail/EpisodeDetailFragment.kt
+++ app/src/main/java/com/mirtneg/rickandmorty/ui/episodedetail/EpisodeDetailFragment.kt
@@ -1,9 +1,13 @@
 package com.mirtneg.rickandmorty.ui.episodedetail
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.findViewTreeOnBackPressedDispatcherOwner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -50,7 +54,9 @@ class EpisodeDetailFragment : Fragment() {
         }
 
         binding.backButton.setOnClickListener {
-            findNavController().navigate(R.id.action_episodeDetailFragment_to_characterDetailFragment)
+            val bundle = Bundle()
+            bundle.putString("character_id",adapter.itemClick.toString())
+            findNavController().navigate(R.id.action_episodeDetailFragment_to_characterDetailFragment, bundle)
         }
     }
 
