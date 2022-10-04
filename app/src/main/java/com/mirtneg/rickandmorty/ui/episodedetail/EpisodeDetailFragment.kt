b6533a65d4d146996073390bfcecca2493f07d93
ArianMehana <mehanaarian@gmail.com>
2022-10-04 14:54:11 +0200

    -added EpisodeCharactersAdapter.kt based on CharactersAdapter.kt but with slightly naming changes
    - added 2 new APIs to get Episodes by ID and get Character by ID to show characters that appear in that Episode
    
    - added respective functions to the newly added APIs
    
    - added EpisodeDetailFragment.kt to make it all work together, mostly similar to CharacterDetailFragment.kt but with a change of positions between Episodes and Characters

---
+++ app/src/main/java/com/mirtneg/rickandmorty/ui/episodedetail/EpisodeDetailFragment.kt
@@ -0,0 +1,62 @@
+package com.mirtneg.rickandmorty.ui.episodedetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mirtneg.rickandmorty.R
+import com.mirtneg.rickandmorty.databinding.FragmentEpisodeDetailBinding
+import com.mirtneg.rickandmorty.ui.home.CharactersAdapter
+
+class EpisodeDetailFragment : Fragment() {
+    private lateinit var binding: FragmentEpisodeDetailBinding
+    lateinit var viewModel: EpisodeDetailViewModel
+    lateinit var adapter: EpisodeCharactersAdapter
+    val args by navArgs<EpisodeDetailFragmentArgs>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEpisodeDetailBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity())[EpisodeDetailViewModel::class.java]
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getEpisodeById(args.episodeId)
+        binding.castList.layoutManager = LinearLayoutManager(requireActivity())
+        adapter = EpisodeCharactersAdapter(this::characterClick)
+        binding.castList.adapter = adapter
+        viewModel.episodeResponse.observe(viewLifecycleOwner){
+            with(it){
+                binding.episodeInfo.tagTextView.text = "Episode"
+                binding.episodeInfo.dataTextView.text = episode
+                binding.airDate.tagTextView.text = "Air Date"
+                binding.airDate.dataTextView.text = airDate
+            }
+        }
+
+        viewModel.characterData.observe(viewLifecycleOwner){
+            adapter.characterItem = it
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_episodeDetailFragment_to_characterDetailFragment)
+        }
+    }
+
+    fun characterClick(characterId: String){
+        val bundle = Bundle()
+        bundle.putString("character_id", characterId)
+        findNavController().navigate(R.id.action_episodeDetailFragment_to_characterDetailFragment, bundle)
+    }
+}
